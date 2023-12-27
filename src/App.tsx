e7d398af2edebc765793aa1654f75ac001306460
unknown <quigley.william@gmail.com>
2023-12-27 23:24:26 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { GoogleLogin } from '@react-oauth/google'
+
+interface LoginResponse {
+  
+}
 
 function App() {
+  const handleLoginSuccess = (response: LoginResponse) => {
+    //swap this out for whatever you want to happen when the user logs in (redirect, etc)
+    console.log('Login Successful:', response)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          To configure, you just need to go to the google cloud console and fetch your client ID <br />
+          The google cloud console is currently available here: https://console.cloud.google.com/ <br />
+          Then change the value of clientId in index.tsx to your client ID (and remove this paragraph)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GoogleLogin 
+          onSuccess={handleLoginSuccess} 
+        />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
